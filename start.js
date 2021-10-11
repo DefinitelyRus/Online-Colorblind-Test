45bb1a85c150a843b026d722accd851c1a73120d
Rus <definitelyrus@gmail.com>
2021-10-11 20:59:16 +0800

    more changes

--- start.js
+++ start.js
@@ -1,5 +1,7 @@
-
-//REPLACE THIS
-export function start() {
-  window.location.replace("Questions.html");
+function start() {
+	//REMOVE
+	//document.getElementById("Title").innerHTML = "Hello there";
+	//document.getElementById("Button").style.backgroundColor = "black";
+	
+	window.location.replace("Questions.html");
 }
