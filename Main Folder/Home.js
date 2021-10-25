8af396a38ef182a1950994d9e00793e401e602c7
KQMl <92298529+KQMl@users.noreply.github.com>
2021-10-25 18:04:28 +0800

    revive １０
    pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on pain on

---
+++ Main Folder/Home.js
@@ -0,0 +1,28 @@
+async function start() {
+	//Fade to black
+	//document.getElementById("mainbody").setAttribute("width", "25px")
+	mainBody = document.getElementById("mainBody");
+	
+	//Wait 1 second
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	
+	window.location.href = "Questions.html"	//Temp
+	//window.location.replace("Questions.html");
+	
+	//Fade from black
+	
+	var cbType = [0,0,0,0,0,0,0];
+	sessionStorage.setItem("cbTypeArray", cbType);
+	
+	
+	/* This counts from 0 to 6 when used on cbType[x].
+	 *
+	 * Deuteranomaly	- Bias towards Red
+	 * Protanomaly		- Bias towards Green
+	 * Deuteranopia		- Red is not visible
+	 * Protanopia		- Green is not visible
+	 * Tritanomaly		- Blue & Green and/or Yellow & Red are difficult to differentiate.
+	 * Tritanopia		- Blue & Green, Purple & Red, and Yellow & Pink are indistinguishable.
+	 * Monochromacy		- Only 1 color is visible (black -> 1 color) (Not explicitly black and white)
+	 */
+}
